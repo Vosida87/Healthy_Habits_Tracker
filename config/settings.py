dfa38d479bf149f48a20e137393289ffdf02a4ac
vovai <vova.izmodenov@mail.ru>
2023-10-22 22:47:25 +0500

    set up Docker

--- config/settings.py
+++ config/settings.py
@@ -95,8 +95,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('HOST'),
     }
 }
 
